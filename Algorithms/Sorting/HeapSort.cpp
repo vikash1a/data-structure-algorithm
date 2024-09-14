24ec6164a29793815b03b9cd1259e43b25d88832
Vikash Sinha <vikash1a2b3c@gmail.com>
2024-09-14 10:54:58 +0000

    remove v2

--- Algorithms/Sorting/HeapSort.cpp
+++ Algorithms/Sorting/HeapSort.cpp
@@ -1,51 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-void printArray(vector<int> v){
-    int n = v.size()-1;
-    for(int i=0;i<=n;i++){
-        cout<<v[i]<<" ";
+void print(vector<int> v){
+    for(int k: v){
+        cout<<k<<" ";
     }
     cout<<endl;
+}
+void heapifyHelper(vector<int>& v, int i, int n){
+    int l = 2*i+1, r = 2*i+2;
+    int largest = i;
+    if(l<=n && (v[largest]<v[l])){
+        largest = l;
+    }
+    if(r<=n && (v[largest]<v[r])) {
+        largest = r;
+    }
+    if(largest!=i){
+        swap(v[i],v[largest]);
+        heapifyHelper(v,largest,n);
+    }
     return;
 }
-void heapify(vector<int>& v, int i, int n){
-    if(2*i+2 > n)return;
-    int l =2*i+1, r = 2*i+2;
-    
-    // printArray(v);
-    if(v[i] < v[l] && v[l] > v[r]){
-        swap(v[i],v[l]);
-        heapify(v,l,n);
-    }
-    else if(v[i] < v[r]){
-        swap(v[i],v[r]);
-        heapify(v,r,n);
-    }
-    return;
-}
-void heapSort(vector<int>& v){
+void heapify(vector<int>& v){
     int n = v.size()-1;
-    //heapify
-    for(int i=0;i<=n/2-1;i++){
-        heapify(v,i,n);
+    for(int i=n/2; i>=0; i--){
+        heapifyHelper(v,i,n);
+        print(v);
     }
-    printArray(v);
-    cout<<"t\n";
-    for(int i=0;i<=n;i++){
-        swap(v[0],v[n-i]);
-        heapify(v,0,n-i-1);
-        printArray(v);
+}
+void heapSort(vector<int> &v){
+    int n = v.size()-1;
+    heapify(v);
+    for(int i=n;i>=0;i--){
+        swap(v[0],v[i]);
+        heapifyHelper(v,0,i-1);
     }
     return;
 }
 
-int main()
-{
-    vector<int> v = { 12, 11, 13, 5, 6, 7 };
-  
+int main(){
+    cout<<"hello world"<<endl;
+    vector<int> v = {4,2,6,3,7,5,4,32,5};
     heapSort(v);
-  
-    cout << "Sorted array is \n";
-    printArray(v);
+    print(v);
+    return 0;
 }
